9e62eabf84a6438415ece192eb3fa5907d1f3189
Inderpal Singh <inderpal2406@gmail.com>
2019-10-07 18:26:50 +0530

    Update .gitignore and James-chap1 files

--- james-turnbull/chap2-ex1/base/base.tf
+++ james-turnbull/chap2-ex1/base/base.tf
@@ -3,7 +3,7 @@ This part is specifically to demonstrate multi-line comments.
 Let's start. */
 
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 # This is single line comment.
@@ -11,4 +11,8 @@ provider "aws" {
 resource "aws_instance" "base" {
   ami           = "ami-0d729a60"
   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "base" {
+  instance = aws_instance.base.id
 }
