6574e5d1ca82232556fe83424c11e47d937d8d31
Inderpal Singh <inderpal2406@gmail.com>
2019-10-08 16:04:45 +0530

    Added chap 3 examples

---
+++ james-turnbull/chap3-ex1/variables.tf
@@ -0,0 +1,24 @@
+# Defining variables for our terraform configuration
+
+# The var.access_key and var.secret_key have already been defined using AWS CLI.
+
+variable "region" {
+  type        = string
+  description = "To hold the AWS region name."
+}
+
+variable "key_name" {
+  type        = string
+  description = "The ssh key pair to get ssh access to resources."
+}
+
+variable "ami" {
+  type        = map(string)
+  description = "A map of AMIs"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "To hold AWS instance type."
+  default     = "t2.micro"
+}
---
+++ james-turnbull/chap3-ex1/web.tf
@@ -0,0 +1,116 @@
+# This file contains definitions for cloud provider and resources that will be used in our infrastructure.
+
+# Defining provider block
+
+provider "aws" {
+  region = var.region
+}
+
+# Defining AWS VPC
+
+module "vpc_basic" {
+  source        = "./vpc_basic"
+  name          = "web"
+  cidr          = "10.0.0.0/16"
+  pubic_subnet  = "10.0.1.0/24"
+}
+
+# Defining AWS EC2 instances
+
+resource "aws_instance" "web" {
+  ami                         = var.ami[var.region]
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = module.vpc_basic.public_subnet_id
+  associate_public_ip_address = true
+  user_data                   = file("files/web_bootstrap.sh")
+  vpc_security_group_ids      = ["aws_security_group.web_host_sg.id"]
+  tags                        = {
+    Name = "web-${format("%03d", count.index + 1)}"
+  }
+  count                       = length(var.instance_ips)
+}
+
+# Defining AWS ELB
+
+resource "aws_elb" "web" {
+  name                        = "web-elb"
+  subnets                     = module.vpc_basic.public_subnet_id
+  security_groups             = aws_security_group.web_inbound_sg.id
+
+  listener {
+  instance_port     = 80
+  instance_protocol = http
+  lb_port           = 80
+  lb_protocol       = http
+  }
+
+  # The instances are registered automatically
+  instances         = aws_instance.web[*].id
+}
+
+# Defining first AWS security group
+
+resource "aws_security_group" "web_inbound_sg" {
+  name        = "web_inbound"
+  description = "Allow http from anywhere in the world."
+  vpc_id      = module.vpc_basic.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8
+    to_port     = 8
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0 "]
+  }
+}
+
+# Defining second AWS security group
+
+resource "aws_security_group" "web_host_sg" {
+  name        = "web_host"
+  description = "Allow SSH and HTTP access to web hosts"
+  vpc_id      = module.vpc_basic.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc_basic.cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8
+    to_port     = 8
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
