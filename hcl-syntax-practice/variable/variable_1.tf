1cf0ae6e744af14f4edde4516545002b83f3660a
Inderpal Singh <inderpal2406@gmail.com>
2019-11-10 10:49:06 +0530

    Added HCL syntax practice files

---
+++ hcl-syntax-practice/variable/variable_1.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  type = "string"
+  default = "Inderpal Singh"
+}
+
+
+variable "father_name" {
+  type = "string"
+  default = "Balvinder Singh"
+}
+
+variable "mother_name" {
+  type    = "string"
+  default = "Darshan Kaur"
+}
